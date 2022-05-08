0ec3d1ea6e9b5ccbb70c0868e41e006b66493600
Edoardo Carlesi <gatto@nanowar.it>
2022-05-09 00:38:23 +0200

    cursor

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ import geopandas as gpd
 import pandas as pd
 import shapely
 import folium
+import airportsdata
+from folium.features import DivIcon
 from streamlit_folium import st_folium
 import matplotlib.pyplot as plt
 from ryanair import Ryanair
+import json
 
 
 def find_connected_flights(airport_code, date_from, date_to):
     ryanair = Ryanair("EUR")
     flights = ryanair.get_flights(airport_code, date_from, date_to)
 
+    destinations = []
     for flight in flights:
+        destinations.append(flight.destinationFull)
+        
+    return destinations
 
-            print(flight)
 
-
-
-def geocode_address(row):
+def geocode_address(row=None, address=None):
 
     address = row['City'] + ' ' + row['Country']
-    print(row, address)
     coord = gpd.tools.geocode(address)
 
     return coord['geometry'].values[0]
@@ -31,36 +34,72 @@ def get_nearby_airport_code(city, country):
 
     pass
 
+def convert_date_format(date):
+    return date[-4:] + '-' + date[3:5] + '-' + date[0:2]
+
+
+def time_span(date):
+
+    date0 = date
+    date1 = date
+
+    return date0, date1
+
 
 if __name__ == '__main__':
 
     csv='data/Tour-dates.csv'
-    df = pd.read_csv(csv)
+    df_full = pd.read_csv(csv, sep=';')
+    df = df_full.iloc[0:3]
     pts = []
     
+    # Center the map on some city around the center of Europe
     eu_center = {'Country':'Germany', 'City':'Darmstadt'}
     map_center = geocode_address(eu_center)
-    #print(map_center)
-    m = folium.Map(location=[map_center.y, map_center.x], zoom_start=4) 
+
+    # Create the folium map!
+    m = folium.Map(location=[map_center.y, map_center.x], 
+                    tiles='Stamen Toner',
+                    zoom_start=4) 
+
     st.title('TOUR MANAGERS MAP')
     st.text('Follow and reach Nanowar wherever they may roam!')
     st.header('\n')
 
+    # FIXME: this is going to look for the png file in some streamlit_folium subfolder!!
+    img_path = 'data/N.png'
+    
     for i, row in df.iterrows():
-        #print(row)
         gps = geocode_address(row)
-        #print(gps, gps.x)
-        pts.append([gps.y, gps.x])
-        #fest = row['Event name']
-        #folium.Marker([gps.y, gps.x], tooltip=fest).add_to(m)
+        gps1 = [gps.y, gps.x]
+        text = f"Event: {row['Event name']}\r\n Date : {row['Event date']}"
+    
+        new_date = convert_date_format(row['Event date'])
 
-        #gps = row['gps']
-        folium.Marker([gps.y, gps.x]).add_to(m)
+        # TODO: make this automatic, find all the IATA codes for the nearby airports!
+        iata = row['IATA1']
+        
+        # Find the ryanair destinations
+        dests = find_connected_flights(iata, new_date, new_date)
 
+        # This creates the icon, remember that the img_path is a messy variable
+        icon = DivIcon(icon_size=(64, 64), 
+            icon_anchor=(0, 0), 
+            html="<img src='"+ img_path +"' alt=" + text + ">"
+            )
 
-    df['gps'] = pts
-    #print(df.head())
-    df.to_csv(csv)
+        # Add the marker!
+        folium.Marker(gps1, icon=icon, tooltip=text).add_to(m)
+
+        if len(dests) > 0:
+            for dest in dests:
+                gps2 = gpd.tools.geocode(dest)['geometry'].values[0]
+                gps2 = [gps2.y, gps2.x]
+                print('POINTs:', gps1, gps2)
+                line = [gps1, gps2]
+                #folium.Marker(gps2, tooltip=text).add_to(m)
+                folium.PolyLine(line).add_to(m)
+
     st_data = st_folium(m, width=800, height=600)
 
     
