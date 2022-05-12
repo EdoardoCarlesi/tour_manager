071af557b9e4231f3443777859839a8a93010118
Edoardo Carlesi <gatto@nanowar.it>
2022-05-12 19:21:01 +0200

    time span added

--- main.py
+++ main.py
@@ -14,98 +14,59 @@ import pickle as pkl
 import json
 import datetime
 from datetime import date
+#from state import provide_state
 import os
 
 # Local imports 
 import html_pages
 
+# These variables will be used throughout the script
 airports = airportsdata.load('IATA')
-
 departures = dict()
 cities_list = dict()
 city2airports = dict()
-
 event_airports_list = []
 countries_list = []
 
-
-def safe_call_ryanair_apis(airport_code, date_from, date_to):
- 
-    ryanair = Ryanair("EUR")
-    date_today = str(date.today())
-    f_name = 'ryan_' + airport_code + date_from + date_to + date_today + '.pkl'
-    f_tmp = os.path.join('tmp', f_name)
-
-    if not os.path.isfile(f_tmp):
-        flights = ryanair.get_flights(airport_code, date_from, date_to)
-        
-        with open(f_tmp, 'wb') as f:
-            pkl.dump(flights, f)
-    else:
-        with open(f_tmp, 'rb') as f:
-            flights = pkl.load(f)
-
-    return flights
+# Reset global variables
+def reset_vars():
+    departures.clear()
 
 
+@st.cache
 def safe_call_ryanair_return_apis(airport_code, date_from, date_to, return_from, return_to):
  
     ryanair = Ryanair("EUR")
     date_today = str(date.today())
     f_name = 'ryan_return_' + airport_code + date_from + date_to + date_today + '.pkl'
     f_tmp = os.path.join('tmp', f_name)
+    trips = ryanair.get_return_flights(airport_code, date_from, date_to, return_from, return_to)
 
-    if not os.path.isfile(f_tmp):
-        trips = ryanair.get_return_flights(airport_code, date_from, date_to, return_from, return_to)
+    
+    #if not os.path.isfile(f_tmp):
+    #    trips = ryanair.get_return_flights(airport_code, date_from, date_to, return_from, return_to)
         
-        with open(f_tmp, 'wb') as f:
-            pkl.dump(trips, f)
-    else:
-        with open(f_tmp, 'rb') as f:
-            trips = pkl.load(f)
+    #    with open(f_tmp, 'wb') as f:
+    #        pkl.dump(trips, f)
+    #else:
+    #    with open(f_tmp, 'rb') as f:
+    #        trips = pkl.load(f)
+    
 
     return trips
 
 
-def find_one_way_flights(airport_code, date_from, date_event, date_to):
-    
-    flights = safe_call_ryanair_apis(airport_code, date_from, date_event, date_event, date_to)
-    destinations = []
-    prices = []
-    codes = []
-
-    for flight in flights:
-        codes.append(flight.destination)
-        destinations.append(flight.destinationFull)
-        prices.append(flight.price)
-   
-        if flight.destination in airports.keys():
-            this_city = airports[flight.destination]['city']
-            this_country = airports[flight.destination]['country']
-            countries_list.append(this_country)
-
-            if not this_city in city2airports.keys():
-                city2airports[this_city] = flight.destination
-
-            if this_country in cities_list.keys():
-
-                if not this_city in cities_list[this_country]:
-                    cities_list[this_country].append(this_city)
-
-            else:
-                cities_list[this_country] = [this_city]
-
-    return destinations, prices, codes
-
-
-def find_return_flights(airport_code, date_from, date_event, date_to):
+def find_return_flights(airport_code, date_from, date_to, date_event=None):
     
     destinations = []
     prices = []
     codes = []
 
     # Let's retrieve all the round trips in the desired time span
-    trips = safe_call_ryanair_return_apis(airport_code, date_from, date_event, date_event, date_to)
+    if not date_event == None:
+        trips = safe_call_ryanair_return_apis(airport_code, date_from, date_event, date_event, date_to)
+    else:
+        trips = safe_call_ryanair_return_apis(airport_code, date_from, date_from, date_to, date_to)
     
     # Loop on the trips
     for trip in trips:
@@ -164,15 +125,6 @@ def convert_date_format(date):
     return date[-4:] + '-' + date[3:5] + '-' + date[0:2]
 
 
-def time_span(date):
-    date0 = date
-    date1 = date
-    return date0, date1
-
-def show_airports():
-    print('SHOWING AIRPORTS!')
-
-
 def create_date_range(date, days_before, days_after):
 
     date_base = datetime.datetime.strptime(date, "%d/%m/%Y")
@@ -187,8 +139,15 @@ def create_date_range(date, days_before, days_after):
 def map_init(m, df):
     """ Initialize the map with all the concerts and nearby airports """
 
+    coords = []
     img_path = 'data/N_icon_small.png'
     img_air_path = 'data/B_icon.png'
+
+    # Geolocate all the gigs
+    for i, row in df.iterrows():
+        gps = geocode_address(row)
+        gps1 = [gps.y, gps.x]
+        coords.append(gps1)
 
     for i, row in df.iterrows():
         event = row['Event name']
@@ -202,25 +161,23 @@ def map_init(m, df):
         iatas = [str(row['IATA1']), str(row['IATA2']), str(row['IATA3'])]
         popup_text = html_pages.popup_html(row)
 
-        icon = CustomIcon(img_path, 
-                        icon_size=(27, 38), 
-                        icon_anchor=(13, 40)) 
-
+        icon_show = CustomIcon(img_path, icon_size=(27, 38), icon_anchor=(13, 40)) 
         icon_text = DivIcon(icon_anchor=(0,0),
                             html="<p class='text'><b><em>" + row['City'].replace(' ', '_').replace('-', '_')  + "</em></b></p>")
+    
+
+        concert_group = folium.FeatureGroup(name='Concerts').add_to(m)
+        airport_group = folium.FeatureGroup(name='Airports', show=False).add_to(m)
 
         # Add the marker for the CONCERT location
-        marker_sign = folium.Marker(coords[i], 
-                        icon=icon,  
-                        popup=popup_text,
-                        tooltip=text)
-        marker_sign.add_to(m)
+        concert_marker = folium.Marker(coords[i], icon=icon_show, popup=popup_text, tooltip=text)
+        #concert_group.add_child(concert_marker)
+        concert_marker.add_to(m)
 
         # Add the location's name
-        marker_text = folium.Marker(coords[i], 
-                        popup=popup_text,
-                        icon=icon_text)
-        marker_text.add_to(m)
+        concert_text = folium.Marker(coords[i], popup=popup_text, icon=icon_text)
+        #concert_group.add_child(concert_text)
+        concert_text.add_to(m)
 
         # Find the ryanair destinations
         for iata in iatas:
@@ -229,33 +186,29 @@ def map_init(m, df):
                 
                 if iata in airports.keys():
 
+                    # Initialize looking at all the shows one week before and after the show
                     date_from, date_to = create_date_range(row['Event date'], 7, 7)
                     new_date = convert_date_format(row['Event date'])
-                    dests, prices, codes = find_return_flights(iata, date_from, new_date, date_to)
+                    dests, prices, codes = find_return_flights(iata, date_from, date_to, date_event=new_date)
                     update_departures(codes, dests, prices, iata, event, new_date)
 
                     gps1 = iata2gps(iata)
                     
                     air_name = airports[iata]['name']
-                    text_air = html_pages.flights_html(airports[iata]['name'], dests, prices)
-                    icon_air = CustomIcon(img_air_path,
-                        icon_size=(27, 38), 
-                        icon_anchor=(13, 40)) 
+                    air_text = html_pages.flights_html(airports[iata]['name'], dests, prices)
+                    air_icon = CustomIcon(img_air_path, icon_size=(27, 38), icon_anchor=(13, 40)) 
                     
                     # Add a marker to the nearby airports
-                    marker_flight = folium.Marker(gps1,
-                                                icon=icon_air,
-                                                tooltip=air_name,
-                                                popup=text_air)
+                    air_marker = folium.Marker(gps1, icon=air_icon, tooltip=air_name, popup=air_text)
+                    #airport_group.add_child(air_marker)           
+                    air_marker.add_to(m)           
 
-                    marker_flight.add_to(m)           
+        #folium.LayerControl().add_to(m)
 
 
-def update_flights_time_range(days_before, days_after, df):
 
-    #del departures 
-    departures = dict()
-    
+def update_flights_time_range(days_before, days_after, df, date_event=False):
+
     for i, row in df.iterrows():
         event = row['Event name']
         date = row['Event date']
@@ -266,7 +219,12 @@ def update_flights_time_range(days_before, days_after, df):
                 if iata in airports.keys():
                     date_from, date_to = create_date_range(date, days_before, days_after)
                     new_date = convert_date_format(date)
-                    dests, prices, codes = find_return_flights(iata, date_from, new_date, date_to)
+
+                    if date_event:
+                        dests, prices, codes = find_return_flights(iata, date_from, date_to, date_event=new_date)
+                    else:
+                        dests, prices, codes = find_return_flights(iata, date_from, date_to)
+
                     update_departures(codes, dests, prices, iata, event, new_date)
     return 1
 
@@ -294,7 +252,6 @@ def update_departures(codes, dests, prices, iata, event, date):
             departures[code]['event_airport'] = [iata]
             departures[code]['event_airport_name'] = [airports[iata]]
 
-
 def sort_departures():
 
     countries_fullname = []
@@ -315,17 +272,10 @@ def sort_departures():
     return countries, cities
 
 
-if __name__ == '__main__':
-
+def main():
     csv = 'data/Tour-dates.csv'
     df = pd.read_csv(csv, sep=';')
-    coords = []
-     
-    # Geolocate all the gigs
-    for i, row in df.iterrows():
-        gps = geocode_address(row)
-        gps1 = [gps.y, gps.x]
-        coords.append(gps1)
+    reset_vars()
 
     # Center the map on some city around the center of Europe
     eu_center = {'Country':'Germany', 'City':'Darmstadt'}
@@ -361,24 +311,25 @@ if __name__ == '__main__':
 
 
     # Band logo
-    #st.image('data/Logo-nanowar.png')
+    st.image('data/Logo-nanowar.png')
 
-    logo_link = ''
-    st.markdown(logo_link, unsafe_allow_html=True)
+    #logo_link = ''
+    #st.markdown(logo_link, unsafe_allow_html=True)
 
     # Create the actual streamlit stuff
-    st.markdown('<p class="title">TOUR MANAGERS M-APP</p>', unsafe_allow_html=True)
+    st.markdown('<p class="title">IN THE SKY SCANNER</p>', unsafe_allow_html=True)
     st.markdown('<p class="text2">Catch Nanowar Of Steel on Tour on the wings of a Barbagianni</p>', unsafe_allow_html=True)
     #st.markdown('<p>Catch Nanowar Of Steel on Tour on the wings of a Barbagianni</p>', unsafe_allow_html=True)
 
     # Initialize and plot the map
     map_init(m, df)
     folium_static(m, width=800, height=600)
-    
+
     # Only show these columns in the general table
     col_keep = ['Event date', 'Event name', 'Country', 'City', 'Website']
 
     st.markdown('<p class="shows"><br>TOUR MANAGERS TOUR</p>', unsafe_allow_html=True)
+    print('STREAMLIT TOUR MANAGERS')
     concerts_text = html_pages.concerts_html(df)
     st.write(concerts_text, unsafe_allow_html=True)
     countries, cities = sort_departures()
@@ -392,32 +343,59 @@ if __name__ == '__main__':
     st.markdown('<p class="text2"> See you on the road! </p>', unsafe_allow_html=True)
     
     st.markdown('<p class="text2">Select a timespan to perform the search. How many days before a show do you want to leave? How many days after?</p>', unsafe_allow_html=True)
-    col1, col2, col3, col4  = st.columns([1, 1, 1, 5])
+    col1, col2, col3, col4, col5  = st.columns([1, 1, 3, 3, 2])
     
-    reload_page = 1
+    departures_text = ''
 
-    with col1:
-        days_before = st.selectbox('Before', [7,6,5,4,3,2,1,0])
-    with col2:
-        days_after = st.selectbox('After', [7,6,5,4,3,2,1])
+    #print(departures)
 
-    if days_before + days_after != 14:
-        reload_page = 0
-        reload_page = update_flights_time_range(days_before, days_after, df)
+    def refresh_connections(search_timeframe): 
+        print('Refreshing flight connections...')
 
-    choose_country = st.selectbox('Choose a country for your departure:', countries)
-    
-    if choose_country:
-        choose_city = st.selectbox('Choose a city for your departure: ', cities[choose_country])
+        try:
+            update_flights_time_range(days_before, days_after, df, date_event=search_timeframe)
+            success = True
+        except:
+            departures_text = '<p><b>NO CONNECTIONS AVAILABLE FOR THE SELECTED DATES </b></p>'
+        
+        if success:
+            code = city2airports[choose_city]
 
-        code = city2airports[choose_city]
-
-        if code in departures.keys():
-
-            if reload_page == 1:
+            if code in departures.keys():
                 departures_text = html_pages.departures_html(departures[code])
-                st.write(departures_text, unsafe_allow_html=True)
-    
+
+        return departures_text
+
+    #countries = ['Liechtenstein', 'Austria']
+    #cities = {'Liechtenstein':['Formia'], 'Austria':['Vienna']}
+
+
+    with st.form('country'):
+        choose_country = st.selectbox('Departure country:', countries)
+        out1 = st.form_submit_button('Refresh city list')
+
+
+    with st.form('city'):
+        search_timeframe = st.checkbox('Search over time span (if not selected search exact dates)', value=True)
+        days_before = st.selectbox('Before', [7,6,5,4,3,2,1,0])
+        days_after = st.selectbox('After', [7,6,5,4,3,2,1])
+        choose_city = st.selectbox('Departure city: ', cities[choose_country])
+        out2 = st.form_submit_button('Submit')
+
+        if out2:
+            reset_vars()
+            departures_text = refresh_connections(search_timeframe)
+            #print(departures)
+            st.write(departures_text, unsafe_allow_html=True)
+
+    #st.write(departures_text, unsafe_allow_html=True)
+    #st.stop()
 
     st.markdown('<p class="credits"><br><br><br>Engineered and coded by Gatto Panceri 666, concept by Tiziana Pinessi</p>', unsafe_allow_html=True)
     st.markdown('<p class="credits">For technical questions, check out the freely available <a href="https://github.com/EdoardoCarlesi/tour_manager" target="_blank"> source code </a>or get in touch with the <a href="mailto:gatto@nanowar.it">webmaster</a></p>', unsafe_allow_html=True)
+
+
+if __name__ == '__main__':
+
+    main()
+
